6ff76e5f9616cb7037041d05322c3c147a768822
Noelle-KH <noelletang86@gmail.com>
2026-05-05 08:30:08 +0800

    feat: initial project

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Operations Navigator</h1>
+      <p>Project initialized and cleaned.</p>
+    </div>
+  )
+}
+
+export default App
